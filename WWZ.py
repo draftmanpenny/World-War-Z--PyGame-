65bea50e7d4b97bb9a3f2b1ae1f95bf0276614c5
Sadiki Johnson <johnsonsadiki2gmail.com>
2022-09-04 18:53:08 -0700

    animations'

--- WWZ.py
+++ WWZ.py
@@ -1,4 +1,3 @@
-from tkinter import Y
 import pygame 
 
 # start pygame 
@@ -12,17 +11,29 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('World War Z')
 
 # new surface 
-round1_surface = pygame.image.load("bg1.jpg")
+round1_surface = pygame.image.load('bg1.jpg')
+soldier_surface = pygame.image.load('soldier.png')
+zombie1_surface = pygame.image.load('zombie1.png')
+zombie1_x_pos = 900  # the starting postion of the zombie... width is the refrence for postion on screen 
+#zombie2_surface = pygame.image.load('zombie2.png')
 
 
-
-# keeps game running until user hits "x" 
+# game loop - keeps game running until user hits "x" 
 running = True
 while running:
   for event in pygame.event.get():  # game runs as lone as its "TRUE"
     if event.type == pygame.QUIT: 
       running = False  
   
-  
+  # places images on screen  
   screen.blit(round1_surface, (0,0))
-  pygame.display.flip()
+  screen.blit(soldier_surface, (300,400))
+  screen.blit(zombie1_surface, (zombie1_x_pos,400))
+  zombie1_x_pos -= 1  # adjust the width postion of the zombie creating movement
+  if zombie1_x_pos < -100:
+     zombie1_x_pos = 900
+  else: 
+    pass
+
+  #screen.blit(zombie2_surface, (700,400))
+  pygame.display.update()
