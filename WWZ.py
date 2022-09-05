efac77c876c85482505c175ce52122cebd293c9d
Sadiki Johnson <johnsonsadiki2gmail.com>
2022-09-04 21:39:19 -0700

    update

--- WWZ.py
+++ WWZ.py
@@ -14,6 +14,10 @@ pygame.display.set_caption('World War Z')
 round1_surface = pygame.image.load('bg1.jpg').convert_alpha()
 soldier_surface = pygame.image.load('soldier.png').convert_alpha()
 zombie1_surface = pygame.image.load('zombie1.png').convert_alpha()
+bullet = pygame.image.load('bullet.png').convert_alpha()
+bullet_x_postion = 375
+
+
 zombie1_x_pos = 900  # the starting postion of the zombie... width is the refrence for postion on screen 
 #zombie2_surface = pygame.image.load('zombie2.png')
 
@@ -28,12 +32,18 @@ while running:
   # places images on screen  
   screen.blit(round1_surface, (0,0))
   screen.blit(soldier_surface, (300,400))
+  screen.blit(bullet, (bullet_x_postion,416))
   screen.blit(zombie1_surface, (zombie1_x_pos,400))
-  zombie1_x_pos -= 1  # adjust the width postion of the zombie creating movement
+  zombie1_x_pos -= 1.5  # adjust the width postion of the zombie creating movement
   if zombie1_x_pos < -100:
      zombie1_x_pos = 900
   else: 
     pass
+  if event.type == pygame.KEYDOWN: 
+    if event.key == pygame.K_SPACE: 
+        bullet_x_postion -= 2
+    else: 
+        pass
 
   #screen.blit(zombie2_surface, (700,400))
-  pygame.display.update()
+  pygame.display.flip()
